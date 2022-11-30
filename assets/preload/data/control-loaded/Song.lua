1b7b6e565f4a62ca7e1b9706047d37fa96cf9b8b
TheM3huPaia <118062367+TheM3huPaia@users.noreply.github.com>
2022-11-29 22:29:36 -0400

    Add files via upload

---
+++ assets/preload/data/control-loaded/Song.lua
@@ -0,0 +1,6 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Port Psych Engine", 0, 10, 14); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 21);
+    addLuaText('watermark');
+    setTextColor('watermark', '00004A')
+end
---
+++ assets/preload/data/control-loaded/notes.lua
@@ -0,0 +1,36 @@
+local keepScroll = false
+
+function onCreate()
+	if getPropertyFromClass('ClientPrefs', 'middleScroll') == true then
+		keepScroll = true;
+	elseif getPropertyFromClass('ClientPrefs', 'middleScroll') == false then
+		setPropertyFromClass('ClientPrefs', 'middleScroll', true);
+	end
+end
+
+function onDestroy()
+	if keepScroll == false then
+		setPropertyFromClass('ClientPrefs', 'middleScroll', false);
+	elseif keepScroll == true then
+		keepScroll = false;
+	end
+end
+
+function onUpdate(elapsed)
+-- This is how I do it
+-- Alpha is +16
+-- X is +0
+-- Y is +8
+-- Angle is +24
+
+-- Opponent arrows: 0-3
+-- Player arrows: 4-7
+-- hide and show the opponents arrows
+for i=0,3 do
+-- Alpha is 0 (0 being invisible)
+noteTweenAlpha(i+16, i, math.floor(curStep/9999), 0.3)
+noteTweenAlpha(i+16, i, math.floor(curStep/9999), 4-7)
+end
+end
+
+
