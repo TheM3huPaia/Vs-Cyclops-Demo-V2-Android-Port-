e4caa4657cb69b93f7bd4870bbb08c199d932c7e
TheM3huPaia <118062367+TheM3huPaia@users.noreply.github.com>
2022-12-02 14:06:25 -0400

    Add files via upload

--- assets/preload/data/manipulation/script.lua
+++ assets/preload/data/manipulation/script.lua
@@ -13,17 +13,13 @@ function onCreate()
     setProperty('miss.visible', false)
 end
 
+function onDestroy()
+	setPropertyFromClass("openfl.Lib","application.window.title", "Speed.gif")
+end
+
 function opponentNoteHit()
        health = getProperty('health')
     if getProperty('health') > 0.1 then
        setProperty('health', health- 0.0195);
 	end
-end
-
-function onUpdate(elapsed)
-    songPos = getSongPosition()
-local currentBeat = (songPos/5000)*(curBpm/60)
-local currentBeat2 = (songPos/200)*(curBpm/200)
-setProperty('camFollowPos.x',getProperty('camFollowPos.x') + (math.sin(currentBeat2) * 0.2))
-setProperty('camFollowPos.y',getProperty('camFollowPos.y') + (math.cos(currentBeat2) * 0.2))
 end
