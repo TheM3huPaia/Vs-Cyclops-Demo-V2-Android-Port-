2d37801deb6d325b3652bd9b19f92f62c5abcd48
TheM3huPaia <118062367+TheM3huPaia@users.noreply.github.com>
2022-12-02 15:15:02 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2249,7 +2249,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ '| Ported by Karl | Misses: ' + songMisses
+		+ ' | Ported by Karl | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
