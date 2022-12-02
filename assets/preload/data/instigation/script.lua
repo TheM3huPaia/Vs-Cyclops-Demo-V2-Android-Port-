37587f30f3c44c686b0b6fb8c6a7fe2422c13278
TheM3huPaia <118062367+TheM3huPaia@users.noreply.github.com>
2022-12-02 15:04:17 -0400

    Add files via upload

--- assets/preload/data/instigation/script.lua
+++ assets/preload/data/instigation/script.lua
@@ -10,15 +10,36 @@ function onStartCountdown()
 end
 
 function onCreate()
-    setTextFont("scoreTxt","sonic-the-hedgehog.ttf");
-    setProperty('timeBar.visible', false)
-    setProperty('timeBarBG.visible', false)
     setTextColor('scoreTxt', 'FF0000')
-    setTextColor('timeTxt', 'FF0000')
     setTextColor('tnh', 'FF0000')
     setTextColor('sick', 'FF0000')
     setTextColor('cm', 'FF0000')
     setTextColor('good', 'FF0000')
     setTextColor('bad', 'FF0000')
     setTextColor('shit', 'FF0000')
+    setTextColor('miss', 'FF0000')
+end
+
+function onCreatePost()
+	setProperty('timeBar.color', getColorFromHex('FF0000'))
+
+end
+
+function onDestroy()
+	setPropertyFromClass("openfl.Lib","application.window.title", "Speed.Gif")
+end
+
+function onUpdate(elapsed)
+	if boyfriendName == 'fatalC' then --replace the name for your character name
+	  if curStep >= 0 then
+  
+		songPos = getSongPosition()
+  
+		local currentBeat = (songPos/1000)*(bpm/80)
+  
+
+		doTweenY(boyfriendTweenY, 'boyfriend', -10 -50*math.sin((currentBeat*0.25)*math.pi),0.001)
+		
+                end  
+        end
 end
